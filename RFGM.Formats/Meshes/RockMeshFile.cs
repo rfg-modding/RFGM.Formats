a1ea28079bc83e8a5fc8ae159ff06994c142c1a4
Luke Money <8206401+Moneyl@users.noreply.github.com>
2025-01-12 18:26:17 -0500

    Port code to read rock meshes (.cstch_pc & .gstch_pc files)

---
+++ RFGM.Formats/Meshes/RockMeshFile.cs
@@ -0,0 +1,58 @@
+using RFGM.Formats.Meshes.Shared;
+using RFGM.Formats.Streams;
+
+namespace RFGM.Formats.Meshes;
+
+public class RockMeshFile
+{
+    public string Name;
+    public bool LoadedCpuFile { get; private set; } = false;
+
+    public MeshConfig Config = new();
+    public List<string> TextureNames = new();
+
+    public RockMeshFile(string name)
+    {
+        Name = name;
+    }
+
+    public void ReadHeader(Stream cpuFile)
+    {
+        //TODO: See if signature + version can check can be added here. Not all formats have them so may not be possible
+
+        //Read the offset of the MeshDataBlock. Always at offset 16
+        cpuFile.Seek(16, SeekOrigin.Begin);
+        uint meshConfigOffset = cpuFile.ReadUInt32();
+        
+        //Read the mesh config
+        cpuFile.Seek(meshConfigOffset, SeekOrigin.Begin);
+        Config.Read(cpuFile);
+        
+        //Read texture names
+        cpuFile.AlignRead(16);
+        uint textureNamesSize = cpuFile.ReadUInt32();
+        TextureNames = cpuFile.ReadSizedStringList(textureNamesSize);
+        
+        LoadedCpuFile = true;
+    }
+    
+    public MeshInstanceData ReadData(Stream gpuFile)
+    {
+        if (!LoadedCpuFile)
+        {
+            throw new Exception("You must call RockMeshFile.ReadHeader() before calling RockMeshFile.ReadData() on RockMeshFile");
+        }
+        
+        //Read index buffer
+        gpuFile.Seek(16, SeekOrigin.Begin);
+        byte[] indices = new byte[Config.NumIndices * Config.IndexSize];
+        gpuFile.ReadExactly(indices);
+        
+        //Read vertex buffer
+        gpuFile.AlignRead(16);
+        byte[] vertices = new byte[Config.NumVertices * Config.VertexStride0];
+        gpuFile.ReadExactly(vertices);
+
+        return new MeshInstanceData(Config, vertices, indices);
+    }
+}
